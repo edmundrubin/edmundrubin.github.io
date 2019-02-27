894985e2215e46549c3837c56f6420ea34c96b1d
edmundrubin <48062957+edmundrubin@users.noreply.github.com>
2019-02-27 23:07:36 +0400

    Create CNAME

---
+++ _site/publish.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo publishing your site right now!
+
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin master
+
